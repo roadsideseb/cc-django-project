b5f7936c43cc67869fad0dacafa59246bf898e83
Sebastian Vetter <sebastian@roadsi.de>
2014-07-23 14:32:44 +1000

    Add urls for users app

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/urls.py
@@ -2,9 +2,14 @@
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html', name='home')),
+
+    url(r'^', include('{{cookiecutter.package_name}}.users.urls',
+                      namespace='users')),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
