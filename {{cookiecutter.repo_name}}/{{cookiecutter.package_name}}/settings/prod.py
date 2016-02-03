708cc1884d17b04be8dfccc7678c8375b18f0fb9
Sebastian Vetter <seb@roadsi.de>
2016-02-02 16:43:42 -0800

    Add django-storages-redux to projects

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/prod.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/settings/prod.py
@@ -21,6 +21,25 @@ class Production(Base):
     DATABASES = values.DatabaseURLValue()
     ALLOWED_HOSTS = ['{{cookiecutter.subdomain}}.herokuapp.com']
 
+    MEDIA_URL = 'https://s3.amazonaws.com/{{cookiecutter.subdomain}}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+    AWS_ACCESS_KEY_ID = values.Value(environ_prefix='')
+    AWS_SECRET_ACCESS_KEY = values.Value(environ_prefix='')
+    AWS_STORAGE_BUCKET_NAME = values.Value(environ_prefix='')
+
+    @property
+    def LIBCLOUD_PROVIDERS(self):
+        if not self.AWS_ACCESS_KEY_ID and not self.AWS_SECRET_ACCESS_KEY:
+            raise ValueError('AWS credentials not provided')
+        return {
+            'default': {
+                'type': 'libcloud.storage.types.Provider.S3',
+                'user': self.AWS_ACCESS_KEY_ID,
+                'key': self.AWS_SECRET_ACCESS_KEY,
+                'bucket': self.AWS_STORAGE_BUCKET_NAME,
+                'secure': True}}
+
     RAVEN_DSN = values.Value()
 
     @property
